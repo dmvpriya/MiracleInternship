05fc09469982a8e9855f69a94c227c2fedc1f043
Mohanaa Vishnu Priya Durla <71810443+dmvpriya@users.noreply.github.com>
2023-07-18 18:19:22 +0530

    Add files via upload

---
+++ HTML/Sample.js
@@ -0,0 +1,7 @@
+function sqaureRoot(form)
+{
+    var val = form.sqrt.value;
+    var a = parseInt(document.getElementById("aa").value);
+    var sqr = Math.sqrt(val);
+    alert("The square root of"+val+"is :"+sqr);
+}
